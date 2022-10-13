ebbc059ab890d9c210016f8877068402bfd953da
jeremymlewis <jeremylewis086@gmail.com>
2022-10-13 10:42:32 -0600

    Create service with Chaz

--- Controllers/LoginController.py
+++ Controllers/LoginController.py
@@ -1,5 +1,5 @@
-from artU_back_end.Model.Service.LoginService import LoginService
-from artU_back_end.Model.User import User
+from Model.Service.LoginService import LoginService
+from Model.User import User
 
 
 class LoginController:
@@ -8,10 +8,11 @@ class LoginController:
     def loginController(self):
         pass
 
-    def signup(self):
-        newUser = User()
-        self.loginService.insertUser(newUser)
-        pass
+    def signup(self, event, context):
+        newUser = User(event['username'], event['password'], event['firstname'], event['lastname'])
+        success = self.loginService.insertUser(newUser)
+        return success #needs to also return the object, and 200/400 instead of bool
 
-    def login(self):
-        pass
+    def login(self, event, context):
+        success = self.loginService.login(self, username, password)
+        return success #needs to also return the object, and 200/400 instead of bool
