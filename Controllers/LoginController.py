9a0dac99180d5aa23c9fc35da873500acb42247f
jeremymlewis <jeremylewis086@gmail.com>
2022-10-21 13:25:21 -0600

    Register uses same 400/200 patern as login

--- Controllers/LoginController.py
+++ Controllers/LoginController.py
@@ -9,28 +9,46 @@ class LoginController:
 
     def signup(self, event, context):
         newUser = User(event['username'], event['password'], event['firstname'], event['lastname'])
-        success = self.loginService.insertUser(newUser)
-        return success #needs to also return the object, and 200/400 instead of bool
+        try:
+            success = self.loginService.insertUser(newUser)
+        except:
+            response = {
+                'statusCode': 400,
+                'isBase64Encoded': False,
+                'headers': { 'Access-Control-Allow-Origin' : '*',
+                'Content-Type': 'application/json' },
+                'body': {"ErrorMessage":"Failed to create new user"}
+            }
+            return response
+        else:
+            response = {
+                'statusCode': 200,
+                'isBase64Encoded': False,
+                'headers': { 'Access-Control-Allow-Origin' : '*',
+                'Content-Type': 'application/json' },
+                'body': success
+            }
+            return response
 
-def login(event, context):
-    loginService = LoginService()
-    try:
-        success = loginService.login(event.get('username'), event.get('password'))
-    except:
-        response = {
-            'statusCode': 400,
-            'isBase64Encoded': False,
-            'headers': { 'Access-Control-Allow-Origin' : '*',
-            'Content-Type': 'application/json' },
-            'body': {"ErrorMessage":"Username or Password is incorrect"}
-        }
-        return response
-    else:
-        response = {
-            'statusCode': 200,
-            'isBase64Encoded': False,
-            'headers': { 'Access-Control-Allow-Origin' : '*',
-            'Content-Type': 'application/json' },
-            'body': success
-        }
-        return response
+    def login(event, context):
+        loginService = LoginService()
+        try:
+            success = loginService.login(event.get('username'), event.get('password'))
+        except:
+            response = {
+                'statusCode': 400,
+                'isBase64Encoded': False,
+                'headers': { 'Access-Control-Allow-Origin' : '*',
+                'Content-Type': 'application/json' },
+                'body': {"ErrorMessage":"Username or Password is incorrect"}
+            }
+            return response
+        else:
+            response = {
+                'statusCode': 200,
+                'isBase64Encoded': False,
+                'headers': { 'Access-Control-Allow-Origin' : '*',
+                'Content-Type': 'application/json' },
+                'body': success
+            }
+            return response
