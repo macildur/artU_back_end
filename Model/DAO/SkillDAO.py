c47f0367636caceaf333541b51a7513455d22a7a
macildur <chaz@rag.net>
2022-11-12 20:41:02 -0700

    Added LessonDAO and SkillDAO

--- Model/DAO/SkillDAO.py
+++ Model/DAO/SkillDAO.py
@@ -7,13 +7,30 @@ class SkillDAO:
     def __init__(self):
         pass
 
-    def get_skill(self, id):
+    def get_matching_images(self, requested_attrs):
+        primary_category = requested_attrs["primary_category"]
+        secondary_categories = requested_attrs["secondary_categories"]
+
         client = boto3.resource('dynamodb')
 
         table = client.Table('skill_table')
+        response = None
+        print("len_sec ", len(secondary_categories), "\n", secondary_categories)
+        if len(secondary_categories) == 2:
+            response = table.scan(
+                FilterExpression=Attr('primary_category').eq(primary_category) & (
+                            Attr("secondary_categories").eq(secondary_categories[0]) | Attr("secondary_categories").eq(
+                        secondary_categories[1]))
+            )
+        elif len(secondary_categories) == 1:
+            response = table.scan(
+                FilterExpression=Attr('primary_category').eq(primary_category) & Attr("secondary_categories").eq(
+                    secondary_categories[0])
+            )
 
-        response = table.scan(
-            FilterExpression=Attr('id').eq(id)
-        )
-        item = response["Items"][0]
-        return item
+        items = response["Items"]
+
+        return {
+            'statusCode': 200,
+            'body': items
+        }
