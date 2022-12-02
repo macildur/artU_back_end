714966ccc8a05fb08665b21607b7deaccb4c1ce3
jeremymlewis <jeremylewis086@gmail.com>
2022-12-02 12:11:36 -0700

    Format controller as AWS

--- Controllers/SkillController.py
+++ Controllers/SkillController.py
@@ -1,31 +1,57 @@
 from random import random
-from Model.Service.SkillService import SkillService
+from SkillService import SkillService
 from Model import Skill
+import simplejson as json
 
 
-class SkillController:
+def getImages(event, context):
+    skillService = SkillService()
+    try:
+        category = json.loads(event.get("body")).get("category")
+        subCategory = json.loads(event.get("body")).get("subCategory")
+        gender = json.loads(event.get("body")).get("gender")
 
-    def __init__(self):
-        self.skillService = SkillService()
+        if category and subCategory and gender:
+            #category, subCategory, gender
+            Skill.validateImageTags(category, subCategory, skillService.getImageTags())
+            images = skillService.getMatchingImages(category, subCategory, gender)
+            random.shuffle(images)
+            response = {
+                'statusCode': 200,
+                'body': json.dumps(images, use_decimal=True)
+            }
+            return response
 
-    def getImages(self, category, subCategory):
-        Skill.validateImageTags(category, subCategory, self.skillService.getImageTags())
-        images = self.skillService.getMatchingImages(category, subCategory, None)
-        random.shuffle(images)
+        elif category and subCategory:
+            #category, subCategory
+            Skill.validateImageTags(category, subCategory, skillService.getImageTags())
+            images = skillService.getMatchingImages(category, subCategory, None)
+            random.shuffle(images)
+            response = {
+                'statusCode': 200,
+                'body': json.dumps(images, use_decimal=True)
+            }
+            return response
 
-        return images
+        else:
+            #mystery (nothing else)
+            tags = skillService.getImageTags()
+            random.shuffle(tags)
+            images = skillService.getMatchingImages(tags[0], tags[1], None)
+            random.shuffle(images)
+            response = {
+                'statusCode': 200,
+                'body': json.dumps(images, use_decimal=True)
+            }
+            return response
 
-    def getImages(self, category, subCategory, gender):
-        Skill.validateImageTags(category, subCategory, self.skillService.getImageTags())
-        images = self.skillService.getMatchingImages(category, subCategory, gender)
-        random.shuffle(images)
-
-        return images
-
-    def getMysteryImages(self):
-        tags = self.skillService.getImageTags()
-        random.shuffle(tags)
-        images = self.skillService.getMatchingImages(tags[0], tags[1], None)
-        random.shuffle(images)
-
-        return images
+    except:
+            response = {
+                "errorType": "Not Found Error",
+                "errorMessage":"No Images found with that tag"
+            }
+            statusCode = 400
+            return {
+                'statusCode': statusCode,
+                'body': json.dumps(response)
+            }
